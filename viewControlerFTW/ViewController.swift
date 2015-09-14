a540f573673f73d8e87a3880e532dd5a4477c2d4
Razzta <Raza@Razas-MacBook-Pro.local>
2015-09-14 00:24:33 -0400

    Assignment 4

--- viewControlerFTW/ViewController.swift
+++ viewControlerFTW/ViewController.swift
@@ -11,8 +11,42 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+
         super.viewDidLoad()
+        view.backgroundColor = UIColor.whiteColor()
+        println("Lifes Awesome")
+
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        println("view will appear")
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        println("view did appear")
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        println("view will layout subviews")
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        println( "view did layout subviews")
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        println("view will disappear")
+    }
+    
+    override func viewDidDisappear(animated: Bool) {
+        super.viewDidDisappear(animated)
+        println("view did disappear")
     }
 
     override func didReceiveMemoryWarning() {
